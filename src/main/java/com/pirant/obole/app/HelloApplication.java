9178e37032bb2a28ea35bade1ed5a5356b1f85dd
pirant0409 <ant.mau.pir@gmail.com>
2025-05-04 21:49:46 +0200

    feat: found devices are clickable buttons

--- src/main/java/com/pirant/obole/app/HelloApplication.java
+++ src/main/java/com/pirant/obole/app/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/pirant/obole/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Find devices");
+        stage.setTitle("Obole");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/pirant/obole/controller/HelloController.java
+++ src/main/java/com/pirant/obole/controller/HelloController.java
@@ -5,32 +5,46 @@ import com.pirant.obole.network.DeviceDiscovery;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 import javax.jmdns.ServiceInfo;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label lookingForDevice;
 
     @FXML
-    protected void onHelloButtonClick() throws IOException, InterruptedException {
-        DeviceDiscovery myDevice = new DeviceDiscovery();
-        myDevice.startService(5000);
+    private VBox devicesContainer;
+
+    @FXML
+    protected void onFindDeviceButtonClick() throws IOException, InterruptedException {
+        lookingForDevice.setText("Looking for device(s)...");
+        DeviceDiscovery myDeviceService = new DeviceDiscovery();
+        myDeviceService.startService(5000);
+        runDiscoveryTask(myDeviceService);
+
+    }
+
+    protected void runDiscoveryTask(DeviceDiscovery myDeviceService) throws IOException, InterruptedException {
 
         Task<Void> discoveryTask = new Task<>() {
             @Override
             protected Void call(){
                 try{
-                    myDevice.discoverServices(updatedList ->{
+                    myDeviceService.discoverServices(updatedList ->{
                         Platform.runLater(() -> {
-                            StringBuilder textToSet = new StringBuilder();
+                            //clearing children to avoid doubles
+                            devicesContainer.getChildren().clear();
                             for (Device d : updatedList){
-                                textToSet.append(d.toString()).append("\n");
+                                createDeviceButton(d);
                             }
-                            welcomeText.setText(textToSet.toString());
                         });
                     });
                 }catch (IOException e){
@@ -43,5 +57,27 @@ public class HelloController {
         Thread discoveryThread = new Thread(discoveryTask);
         discoveryThread.setDaemon(true);
         discoveryThread.start();
+    }
+
+    private void createDeviceButton(Device device) {
+        Button deviceButton = new Button(device.getName() + " (" + device.getAddress() + ")");
+        deviceButton.setOnAction(event -> {
+
+            // setting confirmation alerts for connection
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Connect to device");
+            alert.setHeaderText("Connect to " + device.getName() + " ?");
+            alert.setContentText("Do you want to connect to this device?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK){
+              connectToDevice(device);
+            }
+        });
+        devicesContainer.getChildren().add(deviceButton);
+    }
+
+    private void connectToDevice(Device device) {
+        System.out.println("Connected to device " + device.getName());
     }
 }
